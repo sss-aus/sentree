af6877e0fab30472d48990683616a75dbdc1498d
Sahaj Singh <sahajvirk870@gmail.com>
2025-03-01 15:24:31 +0530

    Added layout for login

---
+++ src/app/Auth/Login/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="">{children}</body>
+    </html>
+  );
+}
